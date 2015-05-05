9409b380d7d71a711893d894c47e828cb7271d6d
Koustuv Sinha <koustuvsinha@hotmail.com>
2015-05-05 21:19:26 +0530

    Added new test cases as per Testing plan

--- test/DataGrid.js
+++ test/DataGrid.js
@@ -1,5 +1,7 @@
 'use strict';
 
 var React = require('react')
+// to make setState work as per http://stackoverflow.com/a/26872245/2861269
+require('react/lib/ExecutionEnvironment').canUseDOM = true
 
 module.exports = React.createFactory(require('../lib'))
