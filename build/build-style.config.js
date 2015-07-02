effa034c22354c0a2739557f89aadc047425460c
zippyui-react <contact@zippyui.com>
2015-07-02 14:48:15 +0300

    build for 1.2.7

--- build/build-style.config.js
+++ build/build-style.config.js
@@ -3,22 +3,22 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: {
-        'index': './index.styl',
-        'index-no-normalize': './style/index.styl'
-    },
-    output: {
-        filename: 'index.css'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.styl$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('[name].css')
+  entry: {
+    'index': './index.styl',
+    'index-no-normalize': './style/index.styl'
+  },
+  output: {
+    filename: 'index.css'
+  },
+  module: {
+    loaders: [
+        {
+          test: /\.styl$/,
+          loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader')
+        }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin('./[name].css')
+  ]
 }
